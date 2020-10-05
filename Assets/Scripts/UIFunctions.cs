d2f9125890b8a1174f007f3689464af1f269e19a
Dale <djs93@njit.edu>
2020-10-04 21:41:21 -0400

    Main Menu and placeholder tutorial and level 1 scenes

---
+++ Assets/Scripts/UIFunctions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIFunctions : MonoBehaviour
+{
+	public void SetScene(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+}
