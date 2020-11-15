78eaabdf42005f80d788f9181645d911bd803649
Dale <djs93@njit.edu>
2020-11-15 04:54:02 -0500

    Health bar look, no logic

---
+++ Assets/minionHealthBar.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class minionHealthBar : MonoBehaviour
+{
+    public GameObject attachObject;
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(attachObject.transform.position.x, transform.position.y, attachObject.transform.position.z);
+    }
+}
