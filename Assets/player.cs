5d7b5b62b08ef0fef09f9c9d00c1f0768b15ea5f
Dale <djs93@njit.edu>
2020-11-14 21:12:48 -0500

    Basic player movement

---
+++ Assets/player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class player : MonoBehaviour
+{
+    private Vector3 target;
+    private NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = transform.position;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            RaycastHit hit;
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                target = hit.point;
+            }
+            agent.SetDestination(target);
+        }
+    }
+}
