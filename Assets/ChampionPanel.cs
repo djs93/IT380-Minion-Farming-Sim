e3256681623eb1784c7591200912f1c0b64fe5d7
Dale <djs93@njit.edu>
2020-11-15 18:36:05 -0500

    Player stat panel updates with initial stats of the player

---
+++ Assets/ChampionPanel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChampionPanel : MonoBehaviour
+{
+    public player playerPlayer;
+    public TextMeshProUGUI damageText;
+    public TextMeshProUGUI rangeText;
+    public TextMeshProUGUI attackSpeedText;
+    public TextMeshProUGUI moveSpeedText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        damageText.text = "<b>Damage:</b><br>"+playerPlayer.damage;
+        rangeText.text = "<b>Attack Range:</b><br>"+playerPlayer.range;
+        attackSpeedText.text = "<b>Attack Speed:</b><br>"+playerPlayer.atkSpeed;
+        moveSpeedText.text = "<b>Move Speed:</b><br>"+playerPlayer.movementSpeed;
+    }
+}
