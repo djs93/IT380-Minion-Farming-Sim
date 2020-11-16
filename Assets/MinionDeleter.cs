0f2183d0cafdd58ba750456924c26975f96792f1
Dale <djs93@njit.edu>
2020-11-15 23:27:25 -0500

    Minions now die at edges of screen

---
+++ Assets/MinionDeleter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionDeleter : MonoBehaviour
+{
+    public bool deleteBlue;
+	// Start is called before the first frame update
+	private void OnTriggerEnter(Collider other)
+	{
+		if (!deleteBlue&&other.gameObject.tag.Equals("Enemy"))
+		{
+			minion mminion = other.gameObject.GetComponent<minion>();
+			if (mminion)
+			{
+				mminion.Die();
+			}
+		}
+		else if(deleteBlue && other.gameObject.tag.Equals("Ally"))
+		{
+			minion mminion = other.gameObject.GetComponent<minion>();
+			if (mminion)
+			{
+				mminion.Die();
+			}
+		}
+	}
+}
