cf609ce519e9bb59dc0bea8dca14075ac3d4f0db
Dale <djs93@njit.edu>
2020-11-15 23:47:06 -0500

    Hide collapse button (can't finish by deadline)

---
+++ Assets/ControlPanel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPanel : MonoBehaviour
+{
+	public RectTransform panel;
+	bool collapsed = false;
+	public void Collapse()
+	{
+		if (!collapsed)
+		{
+			panel.rect.Set(panel.rect.x, -90, panel.rect.width, panel.rect.height);
+			collapsed = true;
+		}
+		else
+		{
+			Expand();
+			collapsed = false;
+		}
+	}
+
+	public void Expand()
+	{ 
+		panel.rect.Set(panel.rect.x, 80, panel.rect.width, panel.rect.height);
+	}
+}
